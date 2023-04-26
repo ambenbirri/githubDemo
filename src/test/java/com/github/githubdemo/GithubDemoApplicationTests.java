a51641380b4714627539f7f93a1af19986b279be
ambenbirri <amine.biribiri@gmail.com>
2023-04-26 17:39:56 +0100

    first commit of spring boot project

---
+++ src/test/java/com/github/githubdemo/GithubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.githubdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
